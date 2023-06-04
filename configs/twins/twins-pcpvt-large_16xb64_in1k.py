b1a15af11bc150f42cc9f39e3a932c791b8e87d2
JacksChoco <ljs8860@toss.im>
2023-06-04 19:27:44 +0900

    dataset 경로 세팅

--- configs/twins/twins-pcpvt-large_16xb64_in1k.py
+++ configs/twins/twins-pcpvt-large_16xb64_in1k.py
@@ -3,5 +3,32 @@ _base_ = ['twins-pcpvt-base_8xb128_in1k.py']
 # model settings
 model = dict(backbone=dict(arch='large'), head=dict(in_channels=512))
 
+data_root="DiseaseID-1"
+
 # dataset settings
-train_dataloader = dict(batch_size=64)
+train_dataloader = dict(
+    batch_size=64,
+    dataset=dict(
+        type='CustomDataset',
+        data_prefix=data_root + '/train',
+        with_label=True,   # or False for unsupervised tasks        
+    )
+)
+
+val_dataloader = dict(
+    dataset=dict(
+        type='CustomDataset',
+        data_prefix=data_root + '/valid',
+        with_label=True,   # or False for unsupervised tasks        
+    )
+)
+
+test_dataloader = dict(
+    dataset=dict(
+        type='CustomDataset',
+        data_prefix=data_root + '/test',
+        with_label=True,   # or False for unsupervised tasks        
+    )
+)
+
+
