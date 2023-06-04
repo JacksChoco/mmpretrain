866cbf131b4fb77b0037ff36986607884d75118f
JacksChoco <ljs8860@toss.im>
2023-06-04 21:14:52 +0900

    train_cfg 비우기

--- configs/_base_/models/twins_pcpvt_base.py
+++ configs/_base_/models/twins_pcpvt_base.py
@@ -24,8 +24,8 @@ model = dict(
         dict(type='TruncNormal', layer='Linear', std=0.02, bias=0.),
         dict(type='Constant', layer='LayerNorm', val=1., bias=0.)
     ],
-    train_cfg=dict(augments=[
-        dict(type='Mixup', alpha=0.8),
-        dict(type='CutMix', alpha=1.0)
-    ]),
+    # train_cfg=dict(augments=[
+    #     dict(type='Mixup', alpha=0.8),
+    #     dict(type='CutMix', alpha=1.0)
+    # ]),
 )
