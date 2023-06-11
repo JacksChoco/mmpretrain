ff779bd8f0bfbba3549cdce2d4f4290919747fc8
JacksChoco <ljs8860@toss.im>
2023-06-11 19:56:12 +0900

    swinv2

--- configs/swin_transformer_v2/swinv2-tiny-w16_16xb64_in1k-256px.py
+++ configs/swin_transformer_v2/swinv2-tiny-w16_16xb64_in1k-256px.py
@@ -6,3 +6,43 @@ _base_ = [
 ]
 
 model = dict(backbone=dict(window_size=[16, 16, 16, 8]))
+
+data_root="caries"
+
+# dataset settings
+train_dataloader = dict(
+    batch_size=4,
+    dataset=dict(
+        type='CustomDataset',
+        data_root=data_root + "/train",
+        ann_file='',
+        data_prefix='',
+        with_label=True,   # or False for unsupervised tasks        
+    )
+)
+
+val_dataloader = dict(
+    batch_size=4,
+    dataset=dict(
+        type='CustomDataset',
+        data_root=data_root + '/valid',
+        ann_file='',
+        data_prefix='',
+        with_label=True,   # or False for unsupervised tasks        
+    )
+)
+
+test_dataloader = dict(
+    batch_size=4,
+    dataset=dict(
+        type='CustomDataset',
+        ann_file='',
+        data_prefix='',
+        data_root=data_root + '/test',
+        with_label=True,   # or False for unsupervised tasks        
+    )
+)
+
+val_evaluator = dict(type='Accuracy', topk=(1, 2))
+
+test_evaluator = val_evaluator
