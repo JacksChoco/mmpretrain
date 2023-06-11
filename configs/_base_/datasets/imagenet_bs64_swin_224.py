efe3a4612f31a5102139a0e62adff26129d3537b
JacksChoco <ljs8860@toss.im>
2023-06-11 16:22:51 +0900

    RandomFlip 만 남기기

--- configs/_base_/datasets/imagenet_bs64_swin_224.py
+++ configs/_base_/datasets/imagenet_bs64_swin_224.py
@@ -14,11 +14,11 @@ bgr_std = data_preprocessor['std'][::-1]
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(
-        type='RandomResizedCrop',
-        scale=224,
-        backend='pillow',
-        interpolation='bicubic'),
+    # dict(
+    #     type='RandomResizedCrop',
+    #     scale=224,
+    #     backend='pillow',
+    #     interpolation='bicubic'),
     dict(type='RandomFlip', prob=0.5, direction='horizontal'),
     # dict(
     #     type='RandAugment',
