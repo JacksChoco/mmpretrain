cf49178eb545cda766f99f6f80b845ed32fbd51d
JacksChoco <ljs8860@toss.im>
2023-06-18 16:53:42 +0900

    batch_size=4

--- configs/_base_/datasets/imagenet_bs32.py
+++ configs/_base_/datasets/imagenet_bs32.py
@@ -24,7 +24,7 @@ test_pipeline = [
 ]
 
 train_dataloader = dict(
-    batch_size=2,
+    batch_size=4,
     num_workers=1,
     dataset=dict(
         type=dataset_type,
@@ -36,7 +36,7 @@ train_dataloader = dict(
 )
 
 val_dataloader = dict(
-    batch_size=2,
+    batch_size=4,
     num_workers=1,
     dataset=dict(
         type=dataset_type,
