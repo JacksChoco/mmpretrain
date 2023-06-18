41fd124e3b2057be665cac51b57880af1083db76
JacksChoco <ljs8860@toss.im>
2023-06-18 19:47:10 +0900

    topk 1

--- configs/_base_/datasets/imagenet_bs32.py
+++ configs/_base_/datasets/imagenet_bs32.py
@@ -46,7 +46,7 @@ val_dataloader = dict(
         pipeline=test_pipeline),
     sampler=dict(type='DefaultSampler', shuffle=False),
 )
-val_evaluator = dict(type='Accuracy', topk=(1, 2))
+val_evaluator = dict(type='Accuracy', topk=(1, 1))
 
 # If you want standard test, please manually configure the test dataset
 test_dataloader = val_dataloader
