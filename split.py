613a5f2ef7b3cfd33ff25143070d3ccb56ee025e
JacksChoco <ljs8860@toss.im>
2023-06-18 19:04:43 +0900

    caries

--- split.py
+++ split.py
@@ -1,6 +1,6 @@
 import splitfolders
 
-input_folder = "/Users/ljs8860/github/mmpretrain/Teeth/Normal"
-output = "/Users/ljs8860/github/mmpretrain/Normal-split/" 
+input_folder = "/Users/ljs8860/github/mmpretrain/Teeth"
+output = "/Users/ljs8860/github/mmpretrain/Split/" 
 
 splitfolders.ratio(input_folder, output=output, seed=42, ratio=(.8, .1, .1)) # ratio of split are in order of train/val/test. You can change to whatever you want. For train/val sets only, you could do .75, .25 for example.
